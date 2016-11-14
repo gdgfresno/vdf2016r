dc31d6a5f647bb0fbafd8ec7dbc654b68795ad76
MrCsabaToth <csaba.toth.us@gmail.com>
2016-11-13 23:41:56 -0800

    Displaying distributions

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-google.charts.load('current', { 'packages': ['bar'] });
+google.charts.load('current', { 'packages': ['corechart', 'bar', 'line'] });
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
   var searchParams = new URLSearchParams(location.search);
-  var $chartDiv = $('chart_div');
+  var $accordion = $('#accordion');
+  var $chartDiv = $('#chart-div');
   if (searchParams.has("uuid")) {
     var uuid = searchParams.get("uuid");
-    var statURL = 'http://atoth.sote.hu/~tocsa/vdf2016r/uuid-' + uuid + '.jsonp';
+    var statURL = 'http://localhost:8887/uuid-' + uuid + '.jsonp';
     $.getJSON(statURL, function(stat) {
       var chartData = [
         [
@@ -17,45 +18,79 @@ function drawChart() {
         ]
       ];
       var rating = stat.rating;
-      var contentRating = rating['content'];
-      if (contentRating.values.length > 0) {
-        chartData.push([
-          'Content',
-          contentRating.median,
-          contentRating.conferenceMedian,
-          contentRating.avg,
-          contentRating.conferenceAvg
-        ]);
-        $('#contentBody').html(JSON.stringify(contentRating.values));
-      } else {
-        chartData.push(['Content', null, null, null, null]);
-      }
-      var presentationRating = rating['presentation'];
-      if (presentationRating.values.length > 0) {
-        chartData.push([
-          'Presentation',
-          presentationRating.median,
-          presentationRating.conferenceMedian,
-          presentationRating.avg,
-          presentationRating.conferenceAvg
-        ]);
-        $('#presentationBody').html(JSON.stringify(presentationRating.values));
-      } else {
-        chartData.push(['Presentation', null, null]);
-      }
-      var venueRating = rating['venue'];
-      if (venueRating.values.length > 0) {
-        chartData.push([
-          'Venue',
-          venueRating.median,
-          venueRating.conferenceMedian,
-          venueRating.avg,
-          venueRating.conferenceAvg
-        ]);
-        $('#venueBody').html(JSON.stringify(venueRating.values));
-      } else {
-        chartData.push(['Venue', null, null]);
-      }
+
+      aggregateData.categoryNames.forEach(function(categoryName) {
+        var categoryRating = rating[categoryName];
+        if (categoryRating.values > 0) {
+          chartData.push([
+            categoryName,
+            categoryRating.median,
+            aggregateData[categoryName].median,
+            categoryRating.avg,
+            aggregateData[categoryName].avg
+          ]);
+
+          $accordion.append(
+            '<div class="panel panel-default">' +
+            '  <div class="panel-heading" role="tab" id="' + categoryName + 'Heading">' +
+            '    <h4 class="panel-title">' +
+            '      <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse' + categoryName + '" aria-expanded="true" aria-controls="collapse' + categoryName + '">' +
+            '        ' + categoryName + ' rating distribution' +
+            '      </a>' +
+            '    </h4>' +
+            '  </div>' +
+            '  <div id="collapse' + categoryName + '" class="panel-collapse collapse" role="tabpanel" aria-labelledby="' + categoryName + 'Heading">' +
+            '    <div class="panel-body">' +
+            '      <div id="' + categoryName + 'Div" class="chart-div">' +
+            '        <p>No data</p>' +
+            '      </div>' +
+            '    </div>' +
+            '  </div>' +
+            '</div>'
+          )
+
+          // var distribData = [
+          //   [
+          //     'Rating',
+          //     'Session',
+          //     'Conference (normalized)'
+          //   ]
+          // ];
+          var distribData = new google.visualization.DataTable();
+          distribData.addColumn('string', 'Rating');
+          distribData.addColumn('number', 'Session');
+          distribData.addColumn('number', 'Conference (normalized)');
+          var numRates = aggregateData.ratingTitles.length;
+          sumAggrDistrib = aggregateData[categoryName].distribution.reduce(function(a, b) {return a + b});
+          sumDistrib = categoryRating.distribution.reduce(function(a, b) {return a + b});
+          for (var i = 0; i < numRates; i++) {
+            // distribData.push([
+            //   aggregateData.ratingTitles[i],
+            //   categoryRating.distribution[i],
+            //   aggregateData[categoryName].distribution[i] * sumDistrib / sumAggrDistrib
+            // ]);
+            distribData.addRow([
+              aggregateData.ratingTitles[i],
+              categoryRating.distribution[i],
+              aggregateData[categoryName].distribution[i] * sumDistrib / sumAggrDistrib
+            ]);
+          }
+          // var dData = google.visualization.arrayToDataTable(distribData);
+          // var $chartDiv = $('#' + categoryName + 'Div');
+          var options = {
+            width: $chartDiv.width(),
+            height: $chartDiv.height(),
+            title: stat.title,
+            subtitle: '',
+            curveType: 'function',
+            pointSize: 7
+          }
+          var dChart = new google.visualization.LineChart(document.getElementById(categoryName + 'Div'));
+          dChart.draw(distribData, options);
+        } else {
+          chartData.push([categoryName, null, null, null, null]);
+        }
+      });
       if (stat.comments.length > 0) {
         var commentsHtml = '<ul>';
         stat.comments.forEach(function (comment) {
@@ -73,7 +108,7 @@ function drawChart() {
           subtitle: ''
         }
       }
-      var chart = new google.charts.Bar(document.getElementById('chart_div'));
+      var chart = new google.charts.Bar(document.getElementById('chart-div'));
       chart.draw(data, options);
     }).fail(function() {
       $chartDiv.html("Couldn't download stat. Wrong GUID?");
